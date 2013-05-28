efcb77c5a2df2a6f12eb8dde9493c4bc4574f3c1
Eric Warsaba <ewarsaba@gmail.com>
2013-05-27 22:28:51 -0700

    added random background generator

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var loaded = false;
-
+var bg = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.png", "6.jpg", "7.png", "8.png", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", ];
 $( function() {
 
   $(window).resize(function(){
@@ -21,4 +21,7 @@ $( function() {
 
 $(document).ready(function () {
 	$(window).resize();
+
+	var r = Math.floor(Math.random()*bg.length);
+	$('body').css('backgroundImage',"url('bg/" + bg[r] + "')");
 });
